d93f788dc18b74dfe399abb7af1f42b19a395d80
agu_ <agustinvasquezseckel@gmail.com>
2026-09-21 14:15:45 -0300

    Agregar beta funcional Web Institucional: backend/spring-boot/src/main/java/cl/webinstitucional/WebInstitucionalApplication.java

---
+++ backend/spring-boot/src/main/java/cl/webinstitucional/WebInstitucionalApplication.java
@@ -0,0 +1,11 @@
+package cl.webinstitucional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInstitucionalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebInstitucionalApplication.class, args);
+    }
+}
